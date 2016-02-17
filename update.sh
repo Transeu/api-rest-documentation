86e65628aeb4960decb7536d2ce1ba5a31f54b48
Michał Gaździk <michal.gazdzik@rst.com.pl>
2016-02-17 07:24:41 +0100

    updated update bash script

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $BRANCH == "gh-pages" ]
+   then
+     echo "Please don't try to update main branch directly"
+     exit 0
+fi
+
 cd _src/
 bin/sculpin install
 bin/sculpin generate --env=prod --url=http://transeu.github.io/api-rest-documentation
@@ -7,6 +15,6 @@ cd ..
 cp -rf _src/output_prod/* .
 rm -rf _src/output_*
 git add .
-git commit -m"update documentation content"
+git commit -m"update documentation content after review"
 git push
 echo "Your branch on Github repository has been updated with recent changes"
