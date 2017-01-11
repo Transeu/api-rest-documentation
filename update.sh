733a95298f100293298997c8117a8295c818a6b8
Jakub Niec <jakub.niec@rst.com.pl>
2017-01-11 09:05:57 +0100

    reverting removed code

--- update.sh
+++ update.sh
@@ -14,3 +14,7 @@ bin/sculpin generate --env=prod --url=http://transeu.github.io/api-rest-document
 cd ..
 cp -rf _src/output_prod/* .
 rm -rf _src/output_*
+git add .
+git commit -m"update documentation content after review"
+git push origin $BRANCH
+echo "Your branch on Github repository has been updated with recent changes"
